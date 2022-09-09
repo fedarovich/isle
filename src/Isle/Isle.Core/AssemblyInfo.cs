7aaea50bd096827244c9d13b66924bab8ea142ae
Pavel Fedarovich <p.fedarovich@gmail.com>
2022-09-08 21:27:59 -0700

    Moved InternalsVisibleTo to project files.

--- src/Isle/Isle.Core/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Isle.Core.Tests")]
-[assembly: InternalsVisibleTo("Isle.Extensions.Logging")]
-[assembly: InternalsVisibleTo("Isle.Extensions.Logging.Tests")]
-[assembly: InternalsVisibleTo("Isle.Serilog")]
-[assembly: InternalsVisibleTo("Isle.Serilog.Tests")]
