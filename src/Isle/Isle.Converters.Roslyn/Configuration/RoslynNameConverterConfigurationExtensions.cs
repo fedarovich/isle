65b52f2e6d0fc064b55ae524cb84053b555f2bf7
Pavel Fedarovich <p.fedarovich@gmail.com>
2025-02-04 15:22:50 -0800

    Added Isle.Converters.Roslyn.

---
+++ src/Isle/Isle.Converters.Roslyn/Configuration/RoslynNameConverterConfigurationExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Isle.Converters;
+using Isle.Converters.Roslyn;
+
+namespace Isle.Configuration;
+
+public static class RoslynNameConverterConfigurationExtensions
+{
+    /// <summary>
+    /// Assigns the <see cref="RoslynNameConverter"/>> to the <see cref="IIsleConfigurationBuilder.ValueNameConverter"/> property.
+    /// </summary>
+    /// <exception cref="ArgumentNullException"><paramref name="configure"/> is <see langword="null"/>.</exception>
+    public static IIsleConfigurationBuilder WithRoslynNameConverter(this IIsleConfigurationBuilder builder, Action<IRoslynNameConverterConfigurationBuilder> configure)
+    {
+        if (configure is null)
+            throw new ArgumentNullException(nameof(configure));
+
+        var configBuilder = new RoslynNameConverterConfigurationBuilder();
+        configure(configBuilder);
+        
+        var converter = configBuilder.Build().Convert;
+        builder.ValueNameConverter = converter.WithMemoization(configBuilder.MemoizationCacheSize);
+        return builder;
+    }
+}
